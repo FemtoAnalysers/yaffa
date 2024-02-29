4790a23b9b552ace8a69ff8f332173c7177c0973
Daniel Battistini <60930860+danielbattistini@users.noreply.github.com>
2024-02-29 16:48:20 +0100

    Revert "Make QA plots"

--- install.sh
+++ install.sh
@@ -4,9 +4,3 @@
 
 # Set the git hooks
 git config --local core.hooksPath .githooks/
-
-# Install the executables
-mkdir -p ~/.local/bin/
-
-cp yaffa/bin/yaffa-lint.sh ~/.local/bin/yaffa-lint
-chmod +x ~/.local/bin/yaffa-lint
--- yaffa/bin/yaffa-lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -e RUN_LOCAL=true -e LOG_LEVEL=WARN -e VALIDATE_PYTHON_PYLINT=true -v $PWD:/tmp/lint github/super-linter \
-    &> >(grep -v "fatal: not a git repository (or any of the parent directories): .git")
