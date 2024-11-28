b140b274d587d38fb47c8a247986747bac5c6947
Daniel Battistini <daniel.battistini@cern.ch>
2024-11-28 14:18:01 +0100

    Use vector of pairs for keeping track of the insertion order, treat user's functions as digits

--- yaffa/utils/FitCF.py
+++ yaffa/utils/FitCF.py
@@ -26,21 +26,20 @@ def FitCF(cfg): # pylint disable:missing-function-docstring
     inFile.Close()
 
     fitter = SuperFitter(oObs, 0, 0.5)
-    # fitter.Add('pol1', [
-    #     ("a0", 1.1, 1, -1),
-    #     ("a1", 0.1, 1, -1),
-    #     # ("a2", 0.1, -10, 20),
-    # ])
+    fitter.Add('pol1', [
+        ("a0", 1.05, 1, -1),
+        ("a1", -0.1, 1, -1),
+    ])
 
     fitter.Add('gaus', [
-        ("norm", 0.001, 0, -1),
+        ("norm", 0.001, 0, 1),
         ("mean", 0.2, 1, -1),
-        ("sigma", 0.01, 0, -1),
+        ("sigma", 0.01, 0, -0.2),
     ])
 
     cFit = TCanvas('cFit', '', 600, 600)
 
-    fitter.Fit("1 + gaus", 'MR+')
+    fitter.Fit("gaus + pol1", 'MR+')
 
     cFit.DrawFrame(0, 0.99, 0.5, 1.12)
     fitter.Draw('same')
