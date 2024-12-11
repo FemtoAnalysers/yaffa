c25261e361f6a8ac672c7473d78f06a2f9837d88
danielbattistini <daniel.battistini@cern.ch>
2024-12-11 11:38:49 +0100

    Change order of installation instructions

--- install.sh
+++ install.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Script to install yaffa
-
-if [[ $ALIENVLVL == '' ]]; then
-    echo -e "\033[31mError: this script must be run inside the ALICE environment. Exit!\033[0m"
-    exit 1
-fi
-
-if [[ $YAFFA == '' ]]; then
-    echo -e "\033[31mError: the path to yaffa was not found. Add `export YAFFA=/path/to/yaffa` to your .bashrc. Exit!\033[0m"
-    exit 1
-fi
-
-currentDir=$PWD
-cd $YAFFA
-
-# Set the git hooks
-git config --local core.hooksPath .githooks/
-
-# Install yaffa
-pip3 install -e . || pip3 install --user -e .
-
 # Install the executables
 mkdir -p ~/.local/bin/
 
@@ -33,4 +12,22 @@ chmod +x ~/.local/bin/yaffa-dr
 cp yaffa/bin/yaffa-farm-perf.py ~/.local/bin/yaffa-farm-perf
 chmod +x ~/.local/bin/yaffa-farm-perf
 
-cd $currentDir
+# Set the git hooks
+git config --local core.hooksPath .githooks/
+
+# Check that the path to YAFFA is defined
+if [[ $YAFFA == '' ]]; then
+    echo -e "\033[31mError: the path to yaffa was not found. Add `export YAFFA=/path/to/yaffa` to your .bashrc. Exit!\033[0m"
+    exit 1
+fi
+pushd $YAFFA
+
+if [[ $ALIENVLVL == '' ]]; then
+    echo -e "\033[31mError: this script must be run inside the ALICE environment. Exit!\033[0m"
+    exit 1
+fi
+
+# Install yaffa
+pip3 install -e . || pip3 install --user -e .
+
+popd
