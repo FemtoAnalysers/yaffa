891fab5b0cb178456747d0f4aa81c1227cef0ca4
Daniel Battistini <daniel.battistini@cern.ch>
2025-01-13 13:23:16 +0100

    Separate draw range and fit range

--- yaffa/utils/Observable.h
+++ yaffa/utils/Observable.h
@@ -23,7 +23,7 @@ class Observable : public TObject {
     void Draw(const char* opt = "") const;
 
     // Fit
-    void Fit(TF1 *fFit, const char* opt = "") const;
+    void Fit(TF1 *fFit, const char* opt = "", double xMin=std::nan(""), double xMax=std::nan("")) const;
 
     ClassDef(Observable, 1)
 };
@@ -40,6 +40,15 @@ Observable::Observable(TH1* hObs) { this->fHObs = hObs; }
 void Observable::Draw(const char* opt) const { this->fHObs->Draw(opt); }
 
 // Fit
-void Observable::Fit(TF1 *fFit, const char* opt) const { this->fHObs->Fit(fFit, opt); }
+void Observable::Fit(TF1 *fFit, const char* opt, double xMin, double xMax) const {
+    if (xMin == xMin && xMax == xMax) {
+        this->fHObs->Fit(fFit, opt, "", xMin, xMax);    
+    } else if (xMin != xMin && xMax != xMax) {
+        this->fHObs->Fit(fFit, opt);
+    } else {
+        throw std::invalid_argument("Both xMin and xMax must be specified");
+    }
+}
+
 
 #endif
