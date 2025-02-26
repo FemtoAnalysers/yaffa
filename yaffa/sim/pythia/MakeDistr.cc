4c6b0c27902028abfadd46f5e2ed993b9c25928b
danielbattistini <daniel.battistini@cern.ch>
2025-02-26 15:26:32 +0100

    Extract functions for setting the process and tune

--- yaffa/sim/pythia/MakeDistr.cc
+++ yaffa/sim/pythia/MakeDistr.cc
@@ -67,6 +67,98 @@ std::map<triggers, const char*> triggerNames = {
     {kHM, "HM"},
 };
 
+
+void SetProcess(Pythia8::Pythia &pythia, std::string process) {
+    if (process == "SoftQCD") {
+        pythia.readString("SoftQCD:all = on");
+        return;
+    }
+    
+    if (process == "HardQCD") {
+        pythia.readString("HardQCD:hardccbar = on");
+        pythia.readString("HardQCD:hardbbbar = on");
+        return;
+    }
+    
+    if (process == "NonDiffractive") {
+        pythia.readString("SoftQCD:nonDiffractive = on");
+        return;
+    }
+    
+    throw std::invalid_argument("Process not implemented. Exit!");
+}
+
+void SetTune(Pythia8::Pythia &pythia, std::string tune) {
+    if (tune == "Monash") {
+        pythia.readString(Form("Tune:pp = 14"));
+        return;
+    }
+    
+    if (tune == "CRMode0") {
+        pythia.readString(Form("Tune:pp = 14"));
+        pythia.readString("ColourReconnection:mode = 1");
+        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
+        pythia.readString("ColourReconnection:m0 = 2.9");
+        pythia.readString("ColourReconnection:allowJunctions = on");
+        pythia.readString("ColourReconnection:junctionCorrection = 1.43");
+        pythia.readString("ColourReconnection:timeDilationMode = 0");
+        pythia.readString("StringPT:sigma = 0.335");
+        pythia.readString("StringZ:aLund = 0.36");
+        pythia.readString("StringZ:bLund = 0.56");
+        pythia.readString("StringFlav:probQQtoQ = 0.078");
+        pythia.readString("StringFlav:ProbStoUD = 0.2");
+        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
+        pythia.readString("MultiPartonInteractions:pT0Ref = 2.12");
+        pythia.readString("BeamRemnants:remnantMode = 1");
+        pythia.readString("BeamRemnants:saturation = 5");
+        return;
+    }
+    
+    if (tune == "CRMode2") {
+        pythia.readString(Form("Tune:pp = 14"));
+        pythia.readString("ColourReconnection:mode = 1");
+        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
+        pythia.readString("ColourReconnection:m0 = 0.3");
+        pythia.readString("ColourReconnection:allowJunctions = on");
+        pythia.readString("ColourReconnection:junctionCorrection = 1.20");
+        pythia.readString("ColourReconnection:timeDilationMode = 2");
+        pythia.readString("ColourReconnection:timeDilationPar = 0.18");
+        pythia.readString("StringPT:sigma = 0.335");
+        pythia.readString("StringZ:aLund = 0.36");
+        pythia.readString("StringZ:bLund = 0.56");
+        pythia.readString("StringFlav:probQQtoQ = 0.078");
+        pythia.readString("StringFlav:ProbStoUD = 0.2");
+        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
+        pythia.readString("MultiPartonInteractions:pT0Ref = 2.15");
+        pythia.readString("BeamRemnants:remnantMode = 1");
+        pythia.readString("BeamRemnants:saturation = 5");
+        return;
+    }
+    
+    if (tune == "CRMode3") {
+        pythia.readString(Form("Tune:pp = 14"));
+        pythia.readString("ColourReconnection:mode = 1");
+        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
+        pythia.readString("ColourReconnection:m0 = 0.3");
+        pythia.readString("ColourReconnection:allowJunctions = on");
+        pythia.readString("ColourReconnection:junctionCorrection = 1.15");
+        pythia.readString("ColourReconnection:timeDilationMode = 3");
+        pythia.readString("ColourReconnection:timeDilationPar = 0.073");
+        pythia.readString("StringPT:sigma = 0.335");
+        pythia.readString("StringZ:aLund = 0.36");
+        pythia.readString("StringZ:bLund = 0.56");
+        pythia.readString("StringFlav:probQQtoQ = 0.078");
+        pythia.readString("StringFlav:ProbStoUD = 0.2");
+        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
+        pythia.readString("MultiPartonInteractions:pT0Ref = 2.05");
+        pythia.readString("BeamRemnants:remnantMode = 1");
+        pythia.readString("BeamRemnants:saturation = 5");
+        return;
+    }
+    
+    throw std::invalid_argument("Tune not implemented. Exit!");
+}
+
 /*
 Integral for Blast Wave. Taken from AliPWGFunc.
     r = x[0]
@@ -418,79 +510,8 @@ void MakeDistr(
 
     Pythia8::Pythia pythia;
     pythia.readString("Next:numberShowEvent = 0");
-    // Set process
-    if (cfg["process"].as<std::string>() == "SoftQCD") {
-        pythia.readString("SoftQCD:all = on");
-    } else if (cfg["process"].as<std::string>() == "HardQCD") {
-        pythia.readString("HardQCD:hardccbar = on");
-        pythia.readString("HardQCD:hardbbbar = on");
-    } else if (cfg["process"].as<std::string>() == "NonDiffractive") {
-        pythia.readString("SoftQCD:nonDiffractive = on");
-    } else {
-        std::cout << "\033[31mError: Process not implemented. Exit!\033[0m" << std::endl;
-        exit(1);
-    }
-
-    // Set tune
-    if (cfg["tune"].as<std::string>() == "Monash") {
-        pythia.readString(Form("Tune:pp = 14"));
-    } else if (cfg["tune"].as<std::string>() == "CRMode0") {
-        pythia.readString(Form("Tune:pp = 14"));
-        pythia.readString("ColourReconnection:mode = 1");
-        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
-        pythia.readString("ColourReconnection:m0 = 2.9");
-        pythia.readString("ColourReconnection:allowJunctions = on");
-        pythia.readString("ColourReconnection:junctionCorrection = 1.43");
-        pythia.readString("ColourReconnection:timeDilationMode = 0");
-        pythia.readString("StringPT:sigma = 0.335");
-        pythia.readString("StringZ:aLund = 0.36");
-        pythia.readString("StringZ:bLund = 0.56");
-        pythia.readString("StringFlav:probQQtoQ = 0.078");
-        pythia.readString("StringFlav:ProbStoUD = 0.2");
-        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-        pythia.readString("MultiPartonInteractions:pT0Ref = 2.12");
-        pythia.readString("BeamRemnants:remnantMode = 1");
-        pythia.readString("BeamRemnants:saturation = 5");
-    } else if (cfg["tune"].as<std::string>() == "CRMode2") {
-        pythia.readString(Form("Tune:pp = 14"));
-        pythia.readString("ColourReconnection:mode = 1");
-        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
-        pythia.readString("ColourReconnection:m0 = 0.3");
-        pythia.readString("ColourReconnection:allowJunctions = on");
-        pythia.readString("ColourReconnection:junctionCorrection = 1.20");
-        pythia.readString("ColourReconnection:timeDilationMode = 2");
-        pythia.readString("ColourReconnection:timeDilationPar = 0.18");
-        pythia.readString("StringPT:sigma = 0.335");
-        pythia.readString("StringZ:aLund = 0.36");
-        pythia.readString("StringZ:bLund = 0.56");
-        pythia.readString("StringFlav:probQQtoQ = 0.078");
-        pythia.readString("StringFlav:ProbStoUD = 0.2");
-        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-        pythia.readString("MultiPartonInteractions:pT0Ref = 2.15");
-        pythia.readString("BeamRemnants:remnantMode = 1");
-        pythia.readString("BeamRemnants:saturation = 5");
-    } else if (cfg["tune"].as<std::string>() == "CRMode3") {
-        pythia.readString(Form("Tune:pp = 14"));
-        pythia.readString("ColourReconnection:mode = 1");
-        pythia.readString("ColourReconnection:allowDoubleJunRem = off");
-        pythia.readString("ColourReconnection:m0 = 0.3");
-        pythia.readString("ColourReconnection:allowJunctions = on");
-        pythia.readString("ColourReconnection:junctionCorrection = 1.15");
-        pythia.readString("ColourReconnection:timeDilationMode = 3");
-        pythia.readString("ColourReconnection:timeDilationPar = 0.073");
-        pythia.readString("StringPT:sigma = 0.335");
-        pythia.readString("StringZ:aLund = 0.36");
-        pythia.readString("StringZ:bLund = 0.56");
-        pythia.readString("StringFlav:probQQtoQ = 0.078");
-        pythia.readString("StringFlav:ProbStoUD = 0.2");
-        pythia.readString("StringFlav:probQQ1toQQ0join = 0.0275,0.0275,0.0275,0.0275");
-        pythia.readString("MultiPartonInteractions:pT0Ref = 2.05");
-        pythia.readString("BeamRemnants:remnantMode = 1");
-        pythia.readString("BeamRemnants:saturation = 5");
-    } else {
-        std::cout << "\033[31mError: Tune not implemented. Exit!\033[0m" << std::endl;
-        exit(1);
-    }
+    SetProcess(pythia, cfg["process"].as<std::string>());
+    SetTune(pythia, cfg["tune"].as<std::string>());
 
     triggers trigger;
     if (cfg["trigger"].as<std::string>() == "MB") {
