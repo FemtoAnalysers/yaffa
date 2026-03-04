b396f616fc019a8d8f783e8e349f98dfa9e94923
Daniel Battistini <daniel.battistini@cern.ch>
2026-03-04 10:41:47 +0100

    Configure number of threads at runtime and add consistency tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+pushd ../build
+make || exit 1
+popd
+
+[[ -f ../build/bin/SimulateSource ]] || (echo '[Error] file ../build/bin/SimulateSource does not exist' && exit 1)
+../build/bin/SimulateSource 1 60
 ../scripts/rootdiff.py source.root source_ref.root
+
