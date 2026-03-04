ad5db205c851b9543b71a00a99839dc3b610e6d1
Daniel Battistini <daniel.battistini@cern.ch>
2026-03-04 17:27:35 +0100

    [WIP] Use configuration file and update tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 pushd ../build
+cmake -DLOG_LEVEL=40 .. || exit 1
 make || exit 1
 popd
 
 [[ -f ../build/bin/SimulateSource ]] || (echo '[Error] file ../build/bin/SimulateSource does not exist' && exit 1)
-../build/bin/SimulateSource 1 60
-../scripts/rootdiff.py source.root source_ref.root
+../build/bin/SimulateSource cfg_test_ceca.yaml || exit 1
+../scripts/rootdiff.py source.root source_ref.root || exit 1
 
