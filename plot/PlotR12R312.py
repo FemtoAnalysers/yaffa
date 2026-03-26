9bb15e4c28289a7b235826500e6a6da0394eac90
Daniel Battistini <daniel.battistini@cern.ch>
2026-03-26 17:48:05 +0100

    Write function to file and update radius value

--- plot/PlotR12R312.py
+++ plot/PlotR12R312.py
@@ -1,4 +1,4 @@
-from ROOT import TF2, TCanvas,  gInterpreter, gROOT
+from ROOT import TF2, TCanvas, TFile, gInterpreter, gROOT
 gInterpreter.Declare('#include "../src/RootFunctions.hxx"')
 from ROOT import SourceAAAJC
 
@@ -6,7 +6,7 @@ gROOT.SetBatch(True)
 
 if __name__ == '__main__':
     fSourceAAAJC = TF2('fSourceAAAJC', SourceAAAJC, 0, 8, 0, 8, 1)
-    fSourceAAAJC.SetParameter(0, 1.25)
+    fSourceAAAJC.SetParameter(0, 1.07)
     fSourceAAAJC.SetNpx(100)
     fSourceAAAJC.SetNpy(100)
     fSourceAAAJC.SetTitle(';r_{12} (fm);r_{3,12} (fm);(4#pi)^{2} r_{12}^{2} r_{3,12}^{2} S(r_{12}, r_{3,12})')
@@ -18,3 +18,6 @@ if __name__ == '__main__':
     fSourceAAAJC.Draw('colz')
     cSource.SaveAs('cR12R312.pdf')
     
+    oFile = TFile('Source3B.root', 'recreate')
+    fSourceAAAJC.Write()
+    oFile.Close()
