3b26d7e359346e6b9e7adeebca17baa958639dc2
Daniel Battistini <daniel.battistini@cern.ch>
2026-03-26 11:54:53 +0100

    [WIP] Compute mT sclaing for 2B

--- scripts/ceca/ComputeSource.py
+++ scripts/ceca/ComputeSource.py
@@ -2,11 +2,47 @@ import sys
 
 from ROOT import TFile, gInterpreter, TF1, gROOT, TGraphErrors
 gInterpreter.Declare('#include "../../src/RootFunctions.hxx"')
-from ROOT import SourceAAA
+from ROOT import SourceAAA, SourceGauss
 
 sys.path.append('../../src')
 
-from utils import SliceVertically
+from utils import SliceVertically, FitHistList
+
+def Write(objects):
+    for object in objects:
+        object.Write()
+
+def GetMtScaling(hSources, mTMins, mTMaxs, func, name):
+    gRStar0VsMt = TGraphErrors()
+    gRStar0VsMt.SetName(name)
+
+    if func == 'average':
+        gRStar0VsMt.SetTitle(';m_{T} (GeV);#LT r_{0} #GT (fm)')
+        for iBin, hRStar in enumerate(hSources):
+            rStarAvg = hRStar.GetMean()
+            rStarAvgUnc = hRStar.GetMeanError()
+            gRStar0VsMt.SetPoint(iBin, (mTMaxs[iBin] + mTMins[iBin]) / 2, rStarAvg)
+            gRStar0VsMt.SetPointError(iBin, (mTMaxs[iBin] - mTMins[iBin]) / 2, rStarAvgUnc)
+        return gRStar0VsMt
+    
+    for iBin, hRStar in enumerate(hSources):
+        fSource = TF1(f'fSource_{mTMins[iBin]:.0f}_{mTMins[iBin]:.0f}', func, 0, 20, 1)
+        fSource.SetNpx(10000)
+        fSource.SetParameter(0, 1)
+        fSource.SetParLimits(0, 0.1, 5)
+
+        hRStar.Scale(1./hRStar.GetEntries() / hRStar.GetBinWidth(1))
+        hRStar.Fit(fSource, 'MR+L')
+        hRStar.SetTitle(';r* (fm);Counts')
+        fSource.Write()
+
+        # Fit results
+        rho0 = fSource.GetParameter(0)
+        rho0unc = fSource.GetParError(0)
+
+        gRStar0VsMt.SetPoint(iBin, (mTMaxs[iBin] + mTMins[iBin]) / 2, rho0)
+        gRStar0VsMt.SetPointError(iBin, (mTMaxs[iBin] - mTMins[iBin]) / 2, rho0unc)
+    return gRStar0VsMt
 
 def main(cfg:dict):
     '''
@@ -23,33 +59,49 @@ def main(cfg:dict):
 
     # Load hitograms from file
     hRhoVsMt = inFile.Get('hRhoVsMt')
+    # if hRhoVsMt.Integral() > 0:
+    #     hMt3B = hRhoVsMt.ProjectionX('hMt3B')
+    #     hMt3B.Write()
 
-    hMt = hRhoVsMt.ProjectionX('hMt')
+    #     hRhos = SliceVertically(hRhoVsMt, cfg['mt_bins'], name='hRho')
+
+    #     gRho0VsMt = TGraphErrors()
+    #     gRho0VsMt.SetName('gRho0VsMt')
+    #     for iBin, hRho in enumerate(hRhos):
+    #         fSource = TF1(f'fSource_{mTMins[iBin]:.0f}_{mTMins[iBin]:.0f}', SourceAAA, 0, 20, 1)
+    #         fSource.SetNpx(10000)
+    #         fSource.SetParameter(0, 1)
+            
+    #         hRho.Scale(1./hRho.GetEntries() / hRho.GetBinWidth(1))
+    #         hRho.Fit(fSource, 'MR+L')
+    #         hRho.SetTitle(';#rho* (fm);Counts')
+    #         hRho.Write()
+    #         fSource.Write()
+
+    #         # Fit results
+    #         rho0 = fSource.GetParameter(0)
+    #         rho0unc = fSource.GetParError(0)
+
+    #         gRho0VsMt.SetPoint(iBin, (mTMaxs[iBin] + mTMins[iBin]) / 2, rho0)
+    #         gRho0VsMt.SetPointError(iBin, (mTMaxs[iBin] - mTMins[iBin]) / 2, rho0unc)
+    #     gRho0VsMt.SetTitle(';m_{T} (GeV);#rho_{0} (fm)')
+    #     gRho0VsMt.Write()
+    
+    # mT scaling for 2B
+    hRStarVsMt = inFile.Get('hRStarVsMt')
+    hMt = hRStarVsMt.ProjectionX('hMt')
     hMt.Write()
+    
+    hRStars = SliceVertically(hRStarVsMt, cfg['mt_bins'], name='hRStar')
+    gRStar0VsMt = GetMtScaling(hRStars, mTMins, mTMaxs, SourceGauss, 'gRStarVsMt')
+    Write(hRStars)
+    gRStar0VsMt.Write()
 
-    hRhos = SliceVertically(hRhoVsMt, cfg['mt_bins'], name='hRho')
+    # Method = average
+    gAvgRStar0VsMt = GetMtScaling(hRStars, mTMins, mTMaxs, 'average', 'gAvgRStarVsMt')
+    gAvgRStar0VsMt.Write()
 
-    gRho0VsMt = TGraphErrors()
-    gRho0VsMt.SetName('gRho0VsMt')
-    for iBin, hRho in enumerate(hRhos):
-        fSource = TF1(f'fSource_{mTMins[iBin]:.0f}_{mTMins[iBin]:.0f}', SourceAAA, 0, 20, 1)
-        fSource.SetNpx(10000)
-        fSource.SetParameter(0, 1)
-        
-        hRho.Scale(1./hRho.GetEntries() / hRho.GetBinWidth(1))
-        hRho.Fit(fSource, 'MR+')
-        hRho.SetTitle(';#rho* (fm);Counts')
-        hRho.Write()
-        fSource.Write()
-
-        # Fit results
-        rho0 = fSource.GetParameter(0)
-        rho0unc = fSource.GetParError(0)
-
-        gRho0VsMt.SetPoint(iBin, (mTMaxs[iBin] + mTMins[iBin]) / 2, rho0)
-        gRho0VsMt.SetPointError(iBin, (mTMaxs[iBin] - mTMins[iBin]) / 2, rho0unc)
-    gRho0VsMt.SetTitle(';m_{T} (GeV);#rho_{0} (fm)')
-    gRho0VsMt.Write()
+    oFile.Close()
 
 if __name__ == '__main__':
     import argparse
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@
 A mix of utility functions
 '''
 
+from ROOT import TGraphErrors, TF1
+
 def SliceVertically(hist, edges, name=None):
     '''
     Slice a TH2 vertically (ProjectionY) and return the list of slices
@@ -18,3 +20,30 @@ def SliceVertically(hist, edges, name=None):
         lastBin = hist.GetXaxis().FindBin(upEdge * 0.9999)
         slices.append(hist.ProjectionY(f'{name}_{lowEdge:.0f}_{upEdge:.0f}', firstBin, lastBin))
     return slices
+
+def FitHistList(hList:list, edges:list, func, nPar, range, name=None, normalize=False) -> TGraphErrors:
+    gParameter = TGraphErrors()
+    gParameter.SetName(name if name else 'gParameter')
+    for iBin, hist in enumerate(hList):
+        fFunc = TF1(f'f{iBin}', func, *range, nPar)
+        fFunc.SetNpx(10000)
+        for iPar in range(nPar):
+            fFunc.SetParameter(iPar, 1)
+
+        if normalize:
+            hist.Scale(1./hist.GetEntries() / hist.GetBinWidth(1))
+ 
+        hist.Fit(fFunc, 'MR+')
+        hist.SetTitle(';#rho* (fm);Counts')
+        hist.Write()
+        fFunc.Write()
+
+        # Fit results
+        par = fFunc.GetParameter(0)
+        parUnc = fFunc.GetParError(0)
+
+        gParameter.SetPoint(iBin, (edges[iBin] + edges[iBin + 1]) / 2, par)
+        gParameter.SetPointError(iBin, (edges[iBin] - edges[iBin + 1]) / 2, parUnc)
+    # gParameter.SetTitle(';m_{T} (GeV);#rho_{0} (fm)')
+    gParameter.Write()
+    
