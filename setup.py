ae7e89def1d52d25cd04427084f23905cab829fb
Daniel Battistini <daniel.battistini@cern.ch>
2026-05-18 14:13:41 +0200

    [Fix] Dependencies and correct path to yaffa in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ from setuptools import setup, find_packages
 setup(
    name='yaffa',
    version='0.0.0',
+   package_dir={"": "src/python"},
    description='Yet Another Framework for Femtoscopy Analyses',
    author='Daniel Battistini',
    author_email='daniel.battistini@cern.ch',
    packages=find_packages(),  # To load all the submodules automatically
-   install_requires=['pyyaml'], # External packages as dependencies
+   install_requires=['pyyaml', 'pytest', 'dotenv', 'rich', 'numexpr', 'pylint'], # External packages as dependencies
 )
