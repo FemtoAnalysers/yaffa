f5353c616eaaf6994e44eada8cab25ee14bbdfec
Daniel Battistini <daniel.battistini@cern.ch>
2026-06-01 14:25:27 +0200

    Plot 3B source for AAA systems

--- plot/PlotSource.py
+++ plot/PlotSource.py
@@ -4,7 +4,7 @@ import argparse
 
 from ROOT import TF2, TCanvas, TFile, TF1, gInterpreter, gROOT
 gInterpreter.Declare('#include "../src/cpp/RootFunctions.hxx"')
-from ROOT import SourceAAAJC, SourceGauss
+from ROOT import SourceAAAJC, SourceGauss, SourceAAA
 
 
 from yaffa import utils
@@ -31,8 +31,6 @@ if __name__ == '__main__':
         fSource.SetParameter(0, args.r0)
         fSource.SetNpx(1000)
         fSource.SetTitle(';r* (fm); 4#pi r*^{2} S(r*)')
-        fSource.Draw('colz')
-
     elif args.system == 'aaa' and args.jc:
         cSource.SetRightMargin(0.22)
         fSource = TF2('fSource', SourceAAAJC, 0, 8, 0, 8, 1)
@@ -42,6 +40,12 @@ if __name__ == '__main__':
         fSource.SetTitle(';r_{12} (fm);r_{3,12} (fm);(4#pi)^{2} r_{12}^{2} r_{3,12}^{2} S(r_{12}, r_{3,12})')
         fSource.GetZaxis().SetTitleOffset(1.7)
         fSource.Draw('colz')
+    elif args.system == 'aaa':
+        fSource = TF1('fSource', SourceAAA, 0, 8, 1)
+        fSource.SetParameter(0, args.r0)
+        fSource.SetNpx(1000)
+        fSource.SetTitle(';#rho (fm); #pi^{3} #rho^{5} S(#rho)')
+        fSource.Draw()
         
     else:
         raise NotImplementedError('Source for this system is not implemented')
