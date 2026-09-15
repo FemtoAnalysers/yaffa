4c481966f3858dc573ba13d3d136d7f1495e2fcb
Daniel Battistini <daniel.battistini@cern.ch>
2026-09-15 10:56:24 +0200

    [Fix] Avoid using radii = 1 in tests which might fail to detect norm issues

--- tests/test_source_functions.py
+++ tests/test_source_functions.py
@@ -37,19 +37,19 @@ EPSILON = 1.e-12
 
 def test_normalization_SourceAAA():
     fSourceAAA = TF1('fSourceAAA', SourceAAA, 0, 1000, 1)
-    fSourceAAA.SetParameter(0, 1)
+    fSourceAAA.SetParameter(0, 2.5)
     fSourceAAA.SetNpx(100000)
     assert abs(fSourceAAA.Integral(0, 1000) - 1) < EPSILON
 
 def test_normalization_SourceGauss():
     fSourceGauss = TF1('fSourceGauss', SourceGauss, 0, 1000, 1)
-    fSourceGauss.SetParameter(0, 1)
+    fSourceGauss.SetParameter(0, 1.3)
     fSourceGauss.SetNpx(100000)
     assert abs(fSourceGauss.Integral(0, 1000) - 1) < EPSILON
 
 def test_normalization_SourceAAAJC():
     fSourceAAAJC = TF2('fSourceAAAJC', SourceAAAJC, 0, 50, 0, 50, 1)
-    fSourceAAAJC.SetParameter(0, 1)
+    fSourceAAAJC.SetParameter(0, 1.2)
     fSourceAAAJC.SetNpx(10000)
     print("---> ", fSourceAAAJC.Integral(0., 50, 0., 50))
     assert abs(fSourceAAAJC.Integral(0., 50, 0., 50, EPSILON) - 1) < EPSILON
@@ -57,21 +57,21 @@ def test_normalization_SourceAAAJC():
 def test_normalization_SourceCountsAAA():
     fSourceCountsAAA = TF1('fSourceCountsAAA', SourceCountsAAA, 0, 1000, 2)
     fSourceCountsAAA.SetParameter(0, 1)
-    fSourceCountsAAA.SetParameter(1, 1)
+    fSourceCountsAAA.SetParameter(1, 2.5)
     fSourceCountsAAA.SetNpx(100000)
     assert abs(fSourceCountsAAA.Integral(0, 1000) - 1) < EPSILON
 
 def test_normalization_SourceCountsGauss():
     fSourceCountsGauss = TF1('fSourceCountsGauss', SourceCountsGauss, 0, 1000, 2)
     fSourceCountsGauss.SetParameter(0, 1)
-    fSourceCountsGauss.SetParameter(1, 1)
+    fSourceCountsGauss.SetParameter(1, 1.3)
     fSourceCountsGauss.SetNpx(100000)
     assert abs(fSourceCountsGauss.Integral(0, 1000) - 1) < EPSILON
 
 def test_normalization_SourceCountsAAAJC():
     fSourceCountsAAAJC = TF2('fSourceCountsAAAJC', SourceCountsAAAJC, 0, 50, 0, 50, 2)
     fSourceCountsAAAJC.SetParameter(0, 1)
-    fSourceCountsAAAJC.SetParameter(1, 1)
+    fSourceCountsAAAJC.SetParameter(1, 1.2)
     fSourceCountsAAAJC.SetNpx(10000)
     print("---> ", fSourceCountsAAAJC.Integral(0., 50, 0., 50))
     assert abs(fSourceCountsAAAJC.Integral(0., 50, 0., 50, EPSILON) - 1) < EPSILON
@@ -79,8 +79,8 @@ def test_normalization_SourceCountsAAAJC():
 def test_normalization_SourceCountsAAAppr():
     fSourceCountsAAAppr = TF2("SourceAAAppr", SourceCountsAAAppr, 0, 30, 0, np.pi / 2,  3)
     fSourceCountsAAAppr.SetParameter(0, 1)
-    fSourceCountsAAAppr.SetParameter(1, 1)
-    fSourceCountsAAAppr.SetParameter(2, 2)
+    fSourceCountsAAAppr.SetParameter(1, 1.4)
+    fSourceCountsAAAppr.SetParameter(2, 2.3)
     fSourceCountsAAAppr.SetNpx(10000)
     fSourceCountsAAAppr.SetNpy(10000)
 
@@ -89,8 +89,8 @@ def test_normalization_SourceCountsAAAppr():
 def test_normalization_SourceCountsAAApprHypAngle():
     fSourceCountsAAApprHypAngle = TF1("SourceAAApprHypAngle", SourceCountsAAApprHypAngle, 0, np.pi / 2, 3)
     fSourceCountsAAApprHypAngle.SetParameter(0, 1)
-    fSourceCountsAAApprHypAngle.SetParameter(1, 1)
-    fSourceCountsAAApprHypAngle.SetParameter(2, 2)
+    fSourceCountsAAApprHypAngle.SetParameter(1, 1.4)
+    fSourceCountsAAApprHypAngle.SetParameter(2, 2.3)
     fSourceCountsAAApprHypAngle.SetNpx(100000)
 
     assert abs(fSourceCountsAAApprHypAngle.Integral(0, np.pi / 2, EPSILON) - 1) < EPSILON
@@ -105,8 +105,8 @@ def test_normalization_SourceCountsAAAHypAngle():
 def test_normalization_SourceCountsAAAprrHypAngle():
     fSourceCountsAAAprrHypAngle = TF1("SourceAAAprrHypAngle", SourceCountsAAAprrHypAngle, 0, np.pi / 2, 3)
     fSourceCountsAAAprrHypAngle.SetParameter(0, 1)
-    fSourceCountsAAAprrHypAngle.SetParameter(1, 1)
-    fSourceCountsAAAprrHypAngle.SetParameter(2, 2)
+    fSourceCountsAAAprrHypAngle.SetParameter(1, 1.4)
+    fSourceCountsAAAprrHypAngle.SetParameter(2, 2.3)
     fSourceCountsAAAprrHypAngle.SetNpx(100000)
 
     assert abs(fSourceCountsAAAprrHypAngle.Integral(0, np.pi / 2, EPSILON) - 1) < EPSILON
@@ -117,8 +117,8 @@ def test_normalization_SourceCountsAAAGaussResonancesHypAngle():
     )
     fSourceCountsAAAGaussResonancesHypAngle.SetParameter(0, 1)
     fSourceCountsAAAGaussResonancesHypAngle.SetParameter(1, 0.3578)
-    fSourceCountsAAAGaussResonancesHypAngle.SetParameter(2, 1)
-    fSourceCountsAAAGaussResonancesHypAngle.SetParameter(3, 2)
+    fSourceCountsAAAGaussResonancesHypAngle.SetParameter(2, 1.4)
+    fSourceCountsAAAGaussResonancesHypAngle.SetParameter(3, 2.3)
     fSourceCountsAAAGaussResonancesHypAngle.SetNpx(100000)
 
     assert abs(fSourceCountsAAAGaussResonancesHypAngle.Integral(0, np.pi / 2, EPSILON) - 1) < EPSILON
